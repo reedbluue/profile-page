e605bd37dbf29576c675cc9d096af2a5473924f3
Igor Oliveira <i.oliveira@email.com>
2024-04-16 22:51:42 -0300

    Changed to /docs

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,4 @@
-import {createBrowserRouter} from "react-router-dom";
+import {createHashRouter} from "react-router-dom";
 import {BasePage} from "../pages/BasePage.tsx";
 import {ProfilePage} from "../pages/ProfilePage.tsx";
 import {TerminalProvider} from "../providers/TerminalProvider.tsx";
@@ -6,7 +6,7 @@ import {ErrorPage} from "../pages/ErrorPage.tsx";
 import {StackPage} from "../pages/StackPage.tsx";
 import {ContatoPage} from "../pages/ContatoPage.tsx";
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: "/",
     element: <TerminalProvider><BasePage/></TerminalProvider>,
